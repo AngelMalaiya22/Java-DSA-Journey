2455e29322678619eb7de6f1b9f180fa13a37916
Angel Malaiya <2025222124.angel@ug.sharda.ac.in>
2026-05-18 10:43:16 +0530

    Add files via upload

---
+++ JAVA-BASICS/JAVA-BASICS/02-Operators/Find_remainder_of_two_numbers.java
@@ -0,0 +1,13 @@
+package Operators;
+
+public class Find_remainder_of_two_numbers 
+{
+    public static void main(String args[])
+    {
+        int a=20;
+        int b=15;
+        int mod=a%b;
+        System.out.println("The Modulus of a and b "+ mod);
+    }
+    
+}
