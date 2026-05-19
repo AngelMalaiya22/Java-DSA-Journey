deb63e274ce7cbc6d9f6d687d4098672893b408e
Angel Malaiya <2025222124.angel@ug.sharda.ac.in>
2026-05-20 00:54:19 +0530

    Add files via upload

---
+++ JAVA-BASICS/JAVA-BASICS/02-Operators/Calculator.java
@@ -0,0 +1,20 @@
+package Operators;
+
+public class Calculator 
+{
+    public static void main(String args[])
+    {
+        int a=10, b=20;
+        int sum=a+b;
+        int diff=a-b;
+        int product=a*b;
+        int divide=a/b;
+        int mod=a%b;
+        System.out.println("The sum of a and b "+sum);
+        System.out.println("The difference of a and b "+diff);
+        System.out.println("The product of a and b "+product);  
+        System.out.println("The division of a and b "+divide);
+        System.out.println("The modulus of a and b "+mod);
+    }
+    
+}
