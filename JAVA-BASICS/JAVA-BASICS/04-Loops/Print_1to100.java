d36814c8dfec941f775cc1f16209d762fc14f5b6
Angel Malaiya <2025222124.angel@ug.sharda.ac.in>
2026-05-25 00:22:56 +0530

    Add files via upload

---
+++ JAVA-BASICS/JAVA-BASICS/04-Loops/Print_1to100.java
@@ -0,0 +1,13 @@
+package Loops;
+
+public class Print_1to100 
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<101;i++)
+        {
+            System.out.print(i + " ");
+        }
+    }
+    
+}
