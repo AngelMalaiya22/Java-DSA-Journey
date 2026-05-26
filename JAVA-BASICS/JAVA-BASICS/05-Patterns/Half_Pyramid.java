00c07d43d5638ef674439ecf0a7c789dc5dfa5a5
Angel Malaiya <2025222124.angel@ug.sharda.ac.in>
2026-05-26 20:11:11 +0530

    Add files via upload

---
+++ JAVA-BASICS/JAVA-BASICS/05-Patterns/Half_Pyramid.java
@@ -0,0 +1,16 @@
+package Pattern_Based_Programs;
+
+public class Half_Pyramid 
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<5;i++)
+        {
+            for(int j=1;j<i+1;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
