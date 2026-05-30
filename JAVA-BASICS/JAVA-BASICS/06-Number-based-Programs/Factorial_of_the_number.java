8588b98c5dfe6293eaffaf2f78b1a8b86ce9ab6e
Angel Malaiya <2025222124.angel@ug.sharda.ac.in>
2026-05-30 11:03:45 +0530

    Add files via upload

---
+++ JAVA-BASICS/JAVA-BASICS/06-Number-based-Programs/Factorial_of_the_number.java
@@ -0,0 +1,18 @@
+package Numbers_Based_Program;
+import java.util.Scanner;
+public class Factorial_of_the_number 
+{
+    public static void main(String args[])
+    {
+        int num, fact=1;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        num=sc.nextInt();
+        for(int i=1;i<num+1;i++)
+        {
+            fact=fact*i;
+        }
+        System.out.println("The factorial of the number is :"+fact);
+    }
+    
+}
