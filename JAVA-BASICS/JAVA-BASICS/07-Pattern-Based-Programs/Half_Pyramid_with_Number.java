c0f8b210045bdebf3798aa31e18aefa14bd59436
Angel Malaiya <2025222124.angel@ug.sharda.ac.in>
2026-06-08 04:19:33 +0530

    Add files via upload

---
+++ JAVA-BASICS/JAVA-BASICS/07-Pattern-Based-Programs/Half_Pyramid_with_Number.java
@@ -0,0 +1,16 @@
+package Pattern_Based_Programs;
+
+public class Half_Pyramid_with_Number 
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<6;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println("");
+        }
+    }
+}
