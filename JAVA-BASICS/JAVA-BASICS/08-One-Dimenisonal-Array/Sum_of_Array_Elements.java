fcb65cd39ea50aca20b1edb54940a1e144cb27dd
Angel Malaiya <2025222124.angel@ug.sharda.ac.in>
2026-06-16 02:27:26 +0530

    Add files via upload

---
+++ JAVA-BASICS/JAVA-BASICS/08-One-Dimenisonal-Array/Sum_of_Array_Elements.java
@@ -0,0 +1,22 @@
+package OneDimenisonal_Array;
+import java.util.Scanner;
+public class Sum_of_Array_Elements 
+{
+    public static void main(String args[])
+    {
+        int sum=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of the array :");
+        int size = sc.nextInt();
+        int arr[]=new int[size];
+        System.out.print("Enter the elements of the array : ");
+        for(int i=0;i<size;i++)
+        {
+            arr[i]=sc.nextInt();
+            sum=sum+arr[i];
+        }
+        System.out.println("Sum of the array elements is : " + sum);
+        
+    }
+    
+}
