0bb84417d46bb721648bf57f36eae2d6e94fe8ef
Angel Malaiya <264592342+AngelMalaiya22@users.noreply.github.com>
2026-09-08 00:02:55 +0530

    Add files via upload

---
+++ JAVA-BASICS/JAVA-BASICS/12-String-Programs/Remove_Spaces.java
@@ -0,0 +1,22 @@
+package string_programs;
+import java.util.Scanner;
+public class Remove_Spaces 
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the string :");
+        String str=sc.nextLine();
+        String newstr="";
+        String space=" ";
+        for(int i=0;i<str.length();i++)
+        {
+            if(str.charAt(i)!= space.charAt(0))
+            {
+                newstr=newstr+str.charAt(i);
+            }
+        }
+        System.out.println("The string after removing spaces is : "+newstr);
+    }
+    
+}
